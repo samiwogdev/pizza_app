79c056371e0b795b19ec942cc0a7d707bee0ca05
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-05-17 14:58:29 +0100

    pizza table created

--- database/migrations/2023_05_17_134651_create_pizzas_table.php
+++ database/migrations/2023_05_17_134651_create_pizzas_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('pizzas', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('descriprtion');
+            $table->integer('small_pizza_price');
+            $table->integer('medium_pizza_price');
+            $table->integer('large_pizza_price');
+            $table->string('category');
+            $table->string('image');
             $table->timestamps();
         });
     }
