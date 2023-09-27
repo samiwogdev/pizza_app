2a02b5abe8ac287c90bacf2f0f8233acbc9124f6
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-09-27 17:47:12 +0100

    load user order

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,55 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row ">
+            <div class="col-md-12">
+                {{-- <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item active" aria-current="page">Orders</li>
+                    </ol>
+                </nav> --}}
+                <div class="card ">
+                    <div class="card-header">Orders
+                        <a style="float:right;" href="{{ route('frontpage') }}"><button class="bnt btn-secondary btn-sm" style="margin-left: 5px;">make order</button></a>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Date/Time</th>
+                                    <th scope="col">Pizza</th>
+                                    <th scope="col">S. pizza</th>
+                                    <th scope="col">M. pizza</th>
+                                    <th scope="col">L. pizza</th>
+                                    <th scope="col">Total($)</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($orders as $key => $order)
+                                <tr>
+                                    <td>{{ __($key + 1 )}}</td>
+                                    <td>{{ $order->date }}/{{ $order->time }}</td>
+                                    <td>{{ $order->pizza->name }}</td>
+                                    <td>{{ $order->small_pizza }}</td>
+                                    <td>{{ $order->medium_pizza }}</td>
+                                    <td>{{ $order->large_pizza }}</td>
+                                    <td>{{ ($order->small_pizza * $order->pizza->small_pizza_price) +
+                                           ($order->medium_pizza * $order->pizza->medium_pizza_price) +
+                                           ($order->large_pizza * $order->pizza->large_pizza_price)
+                                     }}</td>
+                                    <td> {{ $order->description }}</td>
+                                    <td>{{ $order->status}} </td>
+                                </tr>
+                                @endforeach
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
